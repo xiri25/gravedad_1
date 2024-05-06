1023183223a99ac2d112b8fd216009f551c53678
xiri25 <pokesaidesdios@hotmail.com>
2024-05-06 18:02:23 +0200

    ahora la struct esta en otro archivo

--- main.c
+++ main.c
@@ -4,25 +4,6 @@
 
 #define ITERATIONS 1000
 
-// Podria probar con una version de 32bits
-// En teoria si la struct esta compuesta por
-// elementos de un tipo, va a ser compacta en memoria
-typedef struct {
-
-  // Posicion
-  double pos_x;
-  double pos_y;
-
-  // Valor del campo
-  double vector_x;
-  double vector_y;
-
-  // Esto no se si lo quiero guardar aqui
-  double mod_vector;
-  double norm_vec_x;
-  double norm_vec_y;
-} point2d_64;
-
 // Cualquier cosa con masa suficiente como para tenerla en cuenta
 typedef struct {
   double pos_x;
