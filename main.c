a27f9ea2e3870d44c9701a8509c4e7095a2a91ee
xiri25 <pokesaidesdios@hotmail.com>
2024-05-13 05:58:14 +0200

    Unas medidas del tiempo

--- main.c
+++ main.c
@@ -7,6 +7,21 @@
 #include "gravity/gravity.h"
 //#include "setup/setup.h"
 
+#define MEASURE_TIME 1
+
+#if MEASURE_TIME
+#include <time.h>
+
+#define ONE_BILLION 1000000000.0
+
+double now(void) {
+  struct timespec current_time;
+  clock_gettime(CLOCK_REALTIME, &current_time);
+  return current_time.tv_sec + (current_time.tv_nsec / ONE_BILLION);
+}
+#endif
+
+
 #define ITERATIONS 100
 
 cuerpo2d mover_tierra(double radio_orbita, double w, int t) {
@@ -43,6 +58,11 @@ cuerpo2d mover_luna(cuerpo2d tierra, double radio_orbita, double w, int t) {
 }
 
 int main() {
+
+    #if MEASURE_TIME
+    double start_time_main = now();
+    #endif
+
 /*
     sim_info info = {
         .total_iter = 100,
@@ -67,6 +87,10 @@ int main() {
 */
     int size = sizeof(double);
     printf("Un double mide: %d bytes, %d bits\n", size, size * 8);
+
+    #if MEASURE_TIME
+    double start_time_planetas = now();
+    #endif
 
     // Primero vamos a hacer que los "planetas" se muevan
     cuerpo2d sol = {0, 0, 50, 100};
@@ -101,7 +125,17 @@ int main() {
         planetas[i+2] = mover_luna(planetas[i+1], r_orbita_luna_tierra, 0.05, i);
     }
 
+    #if MEASURE_TIME
+    double stop_time_planetas = now();
+    double start_time_planetas_escribir = stop_time_planetas;
+    #endif
+
     escribir_planetas_archivo(planetas, 3, ITERATIONS);
+
+    #if MEASURE_TIME
+    double stop_time_planetas_escribir = now();
+    double start_time_puntos_inicializar = stop_time_planetas_escribir;
+    #endif
 
     //Points, try 1
     grid grid = {20, 5};
@@ -120,17 +154,52 @@ int main() {
 
     inicilizar_points(puntos, grid, ITERATIONS);
 
+    #if MEASURE_TIME
+    double stop_time_puntos_inicializar = now();
+    double start_time_puntos_simular = stop_time_puntos_inicializar;
+    #endif
+
     int puntos_number = grid.height * grid.length * ITERATIONS;
     printf("Hopefully sizeof puntos: %lu\n", puntos_number * sizeof(point2d_64));
 
     //Claramente hay que mejorar the planetas_number situation
     points_simular_secuencial_1(puntos, puntos_number / ITERATIONS, planetas, 3, ITERATIONS); 
 
+    #if MEASURE_TIME
+    double stop_time_puntos_simular = now();
+    double start_time_puntos_escribir = stop_time_puntos_simular;
+    #endif
+
     escribir_puntos_archivo(puntos, puntos_size, &grid, ITERATIONS);
+
+    #if MEASURE_TIME
+    double stop_time_puntos_escribir = now();
+    #endif
 
     //DO NOT FORGET
     free(planetas); // Necesito los planetas para calcular los puntos
     free(puntos);
+
+    #if MEASURE_TIME
+    double stop_time_main = now();
+    double time_main = stop_time_main - start_time_main;
+    printf("Time main: %.10fs\n", time_main);
+
+    double time_planetas = stop_time_planetas - start_time_planetas;
+    printf("Time planetas_calc: %0.10fs\n", time_planetas);
+
+    double time_planetas_escribir = stop_time_planetas_escribir - start_time_planetas_escribir;
+    printf("Time escribir planetas: %0.10fs\n", time_planetas_escribir);
+
+    double time_puntos_inicializar = stop_time_puntos_inicializar - start_time_puntos_inicializar;
+    printf("Time inicializar puntos: %0.10fs\n", time_puntos_inicializar);
+
+    double time_puntos_simular = stop_time_puntos_simular - start_time_puntos_simular;
+    printf("Time simular puntos: %0.10fs\n", time_puntos_simular);
+
+    double time_puntos_escribir = stop_time_puntos_escribir - start_time_puntos_escribir;
+    printf("Time escribir puntos: %0.10fs\n", time_puntos_escribir);
+    #endif
 
     return 0;
 }
